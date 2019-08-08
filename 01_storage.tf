5ae3bdb1d6fc2bd7dbae666e5e31bc5cfd8592e5
Julian Pistorius <julian@julianpistorius.com>
2019-08-07 17:10:52 -0700

    Modified storage configuration to make naming clearer.

--- 01_storage.tf
+++ 01_storage.tf
@@ -1,10 +1,10 @@
-resource "openstack_blockstorage_volume_v2" "postgresql" {
-  name = "${var.env_name}-postgresql"
+resource "openstack_blockstorage_volume_v2" "postgresql_data" {
+  name = "${var.env_name}-postgresql-data"
   size = "${var.postgresql_volume_size}"
 }
 
-resource "openstack_blockstorage_volume_v2" "mongo" {
+resource "openstack_blockstorage_volume_v2" "mongo_data" {
   count = "${var.mongo_count}"
-  name = "${var.env_name}-mongo${count.index}"
+  name = "${var.env_name}-mongo-data${count.index}"
   size = "${var.mongo_volume_size}"
 }
