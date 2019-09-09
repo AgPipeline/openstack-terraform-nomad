3e1966164a878d1c2a51d4b0ad451ae9e26a5683
Julian Pistorius <julian@julianpistorius.com>
2019-09-09 13:49:54 -0700

    Moved Nomad jobs to a new repo.
    See: https://github.com/az-digitalag/pipeline-nomad-jobs

--- nomad-test/docker/clowder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f /home/clowder/RUNNING_PID
-
-#cp /local/custom.conf /home/clowder/custom/custom.conf
-
-python -m SimpleHTTPServer 9000
-
-#exec /home/clowder/bin/clowder -DMONGOUPDATE=1 -DPOSTGRESUPDATE=1 $*
